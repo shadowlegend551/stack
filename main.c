bbfbad2c047f059648b082363948f03bbd98ac7b
shadowlegend551 <oskari.j.lappalainen@gmail.com>
2024-02-11 14:00:35 +0200

    Added comments to main.c

--- main.c
+++ main.c
@@ -5,22 +5,30 @@
 
 int main(void)
 {
-    Stack stack = init_stack(-6);
+    Stack stack = init_stack(0);
 
+    // First string.
     char* string = "Hello World";
+	
+	// Second string.
     char* string2 = "wassup";
 
     int string_length = strlen(string);
     int string2_length = strlen(string2);
 
-    void* result = push(&stack, string, string_length);
-    string = "wassup";
-    result = push(&stack, string2, string2_length);
+    // Push first string.
+    push(&stack, string, string_length);
+	
+	// Push second string.
+    push(&stack, string2, string2_length);
     
-
+    // Pop second string to first variable.
     string = pop(&stack, string2_length);
+	
+	// Pop first string to second variable.
     string2 = pop(&stack, string_length);
     
+	// Display results.
     printf("String1: %s\nString2: %s\n", string, string2);
     return 0;
 }
